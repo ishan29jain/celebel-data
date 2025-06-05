fb91515275cb7f53fe9e2af3643ff3ba60df8290
Ishan Jain <84621870+ishan29jain@users.noreply.github.com>
2025-06-05 21:32:23 +0530

    Update finding_the_percentage.py

--- finding_the_percentage.py
+++ finding_the_percentage.py
@@ -1,1 +1,9 @@
+n = int(input())
+marks = {}
 
+for _ in range(n):
+    parts = input().split()
+    marks[parts[0]] = list(map(float, parts[1:]))
+
+query_name = input()
+print(f"{sum(marks[query_name]) / 3:.2f}")

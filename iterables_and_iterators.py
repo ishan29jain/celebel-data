9098e27007717beedea29409f1f664fb0041814d
Ishan Jain <84621870+ishan29jain@users.noreply.github.com>
2025-06-05 21:33:31 +0530

    Update iterables_and_iterators.py

--- iterables_and_iterators.py
+++ iterables_and_iterators.py
@@ -1,1 +1,10 @@
+from itertools import combinations
 
+n = int(input())
+letters = input().split()
+k = int(input())
+
+combs = list(combinations(letters, k))
+a_count = sum(1 for c in combs if 'a' in c)
+
+print(f"{a_count / len(combs):.4f}")
